a0c16e7094447e92ef01c473ec4caa26e7400208
John Schroeder <john@contactci.co>
2024-03-06 15:40:38 -0500

    Slam the service to until it melts

--- test/surface_motion/main.cpp
+++ test/surface_motion/main.cpp
@@ -3,21 +3,30 @@
 //
 
 #include "haptics.h"
-
+#include <chrono>
 #include <cstdio>
+#include <iostream>
+
+#include <windows.h>
+
+using namespace std::chrono;
 
 int main() {
     start_maestro_detection_service();
     int right = get_right_glove_pointer();
 
     for (int i = 0; ; i++) {
+        std::cout << duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count() << std::endl;
+
         start_haptic_transaction(right);
 
-        set_index_vibration_amplitude(right, ((100 + i) % 255) / 255.0f);
+        set_thumb_vibration_amplitude(right, (i % 50 / 50.0f));
+        set_index_vibration_amplitude(right, (i % 50 / 50.0f));
+        set_middle_vibration_amplitude(right, (i % 50 / 50.0f));
+        set_ring_vibration_amplitude(right, (i % 50 / 50.0f));
+        set_little_vibration_amplitude(right, (i % 50 / 50.0f));
 
         end_haptic_transaction(right);
-
-        getchar();
     }
 
     stop_maestro_detection_service();
