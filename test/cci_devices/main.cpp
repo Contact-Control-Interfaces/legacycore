e15679eb6f15d59de76efc999012f553a9fd045c
Brant Martin <brant@contactci.co>
2024-09-25 15:59:32 -0400

    waveform upload test

--- test/cci_devices/main.cpp
+++ test/cci_devices/main.cpp
@@ -8,23 +8,68 @@
 
 #include <windows.h>
 
+#include "../../ccic/include/contactci.h"
+
 using namespace std::chrono;
 
 int main() {
+    std::cout << "testing wave cache..." << std::endl;
     contactci::MutableHapticSession session;
 
-    for (int i = 0; ; i++) {
-        std::cout << duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count() << std::endl;
-        Sleep(500);
+    Sleep(500);
+    auto left = session.get_left_device();
+    auto right = session.get_right_device();
 
-        auto left = session.get_left_device();
-        auto right = session.get_right_device();
-
-        if (left.has_value())
+        contactci::DeviceDescription *device;
+        if (left.has_value()) {
             std::cout << "left: " << left.value().get_serial_number() << std::endl;
-
-        if (right.has_value())
+            device = &left.value();
+        } else if (right.has_value()) {
             std::cout << "right: " << right.value().get_serial_number() << std::endl;
+            device = &right.value();
+        } else {
+            std::cerr << "No devices connected" << std::endl;
+            return 1;
+        }
+
+    std::vector<float> dummy;
+    for (int i = 0; i < 255; i++) {
+        dummy.push_back(static_cast<float>(i) / 255.f);
+    }
+
+    std::cout << "Transmitting waveform with " << dummy.size() << "values" << std::endl;
+    auto &mgr = session.get_session_device_manager();
+
+    CachedWaveform vout;
+    auto res = mgr.transfer_waveform(*device, 8000, 0, dummy, vout, "testvalue");
+
+    std::cout << "Transmit finished. Got result: ";
+
+    switch (res) {
+        case error:
+            std::cout << "error";
+            break;
+        case indexConflict:
+            std::cout << "idx conflict";
+            break;
+        case noSpace:
+            std::cout << "no space";
+            break;
+        case okay:
+            std::cout << "success";
+            break;
+        default: ;
+    }
+
+    std::cout << std::endl;
+
+    std::cout << "requesting wave table.." << std::endl;
+
+    auto table = mgr.get_cached_waveforms(*device, false);
+
+    std::cout << "table contains " << table.size() << "entries" << std::endl;
+    for(auto wav : table) {
+        std::cout << "index: " << wav.index << " size: " << wav.length << " identifier: " << wav.identifier.value_or("NUL") << std::endl;
     }
 
     return 0;
